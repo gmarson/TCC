b8a4a3368d68cc4239cef9c87a71f3400fc4650a
Gabriel Marson <gabrielmarson95@gmail.com>
2017-02-19 16:45:33 -0300

    Problema da mochila

--- MOEA/src/ProblemKnapsack.java
+++ MOEA/src/ProblemKnapsack.java
@@ -5,29 +5,26 @@ import java.util.ArrayList;
  */
 public class ProblemKnapsack extends Problem{
 
+
     public static ArrayList<KnapsackItem> items = new ArrayList<>();
 
+
     public ProblemKnapsack(){
-        crossover = new CrossoverArithmetic();
+        crossover = new CrossoverBinaryKnapsack();
         Constants.PROBLEM_SIZE = 2;
-        Constants.MAX_MEMBER_VALUE = 10;
-        Constants.MIN_MEMBER_VALUE = -10;
-        Constants.MAX_BINARY_LEN = 11;
-        Constants.QTD_ITEMS = 5;
-        Constants.BAG_CAPACITY = 1000;
+        Constants.QTD_ITEMS = 10;
+        Constants.BAG_CAPACITY = 100;
         this.buildItems();
     }
 
     @Override
     public void evaluateAgainstObjectiveFunctions(Population p)
     {
-        //todo aki eu pego o somatorio dos atributos para um certo tipo de objetivo.
-        /*
-        * se o objetivo eh do tipo um entao eu vejo os items que estao na mochila e somo todos os atributos do tipo 1 de todos os items
-        * analogamente para o tipo 2
-        *
-        * entao eu tenho dois quesitos de melhor opçao de escolha se eu tiver dois objetivos
-        * */
+        for (int i = 0; i < p.population.size(); i++)
+        {
+            Member member = p.population.get(i);
+            this.applyFunctions(member);
+        }
     }
 
     @Override
@@ -39,27 +36,56 @@ public class ProblemKnapsack extends Problem{
         }
 
         return population;
-
-        //PENSOO QUE ESSE VALUE NAO SERVIRA DE NADA MAS EU POSSO TRANSFORMAR O BINARIOS DE TER OU NAO ITEM EM UM NUMERO DECIMAL
-        //SO PRA FICAR COESO MSM
     }
 
     @Override
     public void applyFunctions(Member member)
     {
 
+        member.resultOfFunctions.add(firstFunction(member));
+        member.resultOfFunctions.add(secondFunction(member));
     }
 
     @Override
-    public double firstFunction(int valueOfMember)
-    {
-        return 0;
+    public double firstFunction(Member member) {
+        double firstFunctionValue = 0;
+
+        for (int j = 0; j < Constants.QTD_ITEMS; j++)
+        {
+            if (member.binaryValue.get(j) == 1)
+            {
+                firstFunctionValue += ProblemKnapsack.items.get(j).attributes.get(0);
+            }
+        }
+
+        return firstFunctionValue > Constants.BAG_CAPACITY? (2): (1/firstFunctionValue);
+
+    }
+
+
+    @Override
+    public double secondFunction(Member member) {
+        double secondFunctionValue = 0;
+        for (int j = 0; j < Constants.QTD_ITEMS; j++)
+        {
+            if(member.binaryValue.get(j) == 1)
+            {
+                secondFunctionValue += ProblemKnapsack.items.get(j).attributes.get(1);
+            }
+        }
+
+        if( member.resultOfFunctions.get(0) ==2) return 2;
+
+        return secondFunctionValue == 0? (1/0.1) : (1/secondFunctionValue);
+
     }
 
     @Override
-    public double secondFunction(int valueOfMember)
-    {
-        return 0;
+    public void printResolutionMessage() {
+        System.out.println("Dado os seguintes intens: ");
+        this.printItems();
+        System.out.println("Os melhores indivíduos foram ");
+
     }
 
     public void buildItems()
@@ -69,30 +95,50 @@ public class ProblemKnapsack extends Problem{
         }
     }
 
+    public void printItems()
+    {
+        int i = 0;
+        for(KnapsackItem item: items)
+        {
+            System.out.println("Item "+i);
+            item.printItem();
+            i++;
+        }
+    }
 
     public class KnapsackItem{
         public double weight;
-        public ArrayList<Double> attributes; //lucro e por ai vai
+        public ArrayList<Double> attributes = new ArrayList<>(); //lucro e por ai vai
 
-        private double MAX_ATTRIBUTE_VALUE_FOR_ITEM = 100000;
-        private double MIN_ATTRIBUTE_VALUE_FOR_ITEM = -100000;
+        private double MAX_ATTRIBUTE_VALUE_FOR_ITEM = 10;
+        private double MIN_ATTRIBUTE_VALUE_FOR_ITEM = 1;
         private double MAX_WEIGHT = 100;
         private double MIN_WEIGHT = 1;
 
+        //todo tirar os castings depois
         public KnapsackItem()
         {
-            this.weight =  Utils.getRandomDouble(MIN_WEIGHT,MAX_WEIGHT);
+            this.weight =  Utils.getRandom((int)MIN_WEIGHT,(int)MAX_WEIGHT);
             this.createAttributesOfItems();
         }
 
+
+        //todo tirar os castings depois
         public void createAttributesOfItems()
         {
-            for (int i = 0; i <Constants.PROBLEM_SIZE ; i++) {
-                this.attributes.add(Utils.getRandomDouble(MIN_ATTRIBUTE_VALUE_FOR_ITEM,MAX_ATTRIBUTE_VALUE_FOR_ITEM));
+            this.attributes.add(0,this.weight);
+            for (int i = 1; i <Constants.PROBLEM_SIZE ; i++) {
+                this.attributes.add(i,(double)Utils.getRandom((int)MIN_ATTRIBUTE_VALUE_FOR_ITEM,(int)MAX_ATTRIBUTE_VALUE_FOR_ITEM));
             }
         }
 
-
+        public void printItem()
+        {
+            System.out.println("Peso: "+this.weight);
+            for (int i = 1; i < attributes.size(); i++) {
+                System.out.println("Atributo "+i+" = "+attributes.get(i)+"\n");
+            }
+        }
 
 
     }
